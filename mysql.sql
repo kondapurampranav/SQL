fb99013582156270ccec97c5ccda8bf0c734795c
Pranav kumar reddy k <kondapurampranav@gmail.com>
2026-03-17 00:56:26 +0530

    Added db

---
+++ mysql.sql
@@ -0,0 +1,17 @@
+
+CREATE TABLE customers (
+    id INT PRIMARY KEY,
+    name VARCHAR(50),
+    city VARCHAR(50)
+);
+
+-- INSERTION
+
+INSERT INTO customers (id, name, city) VALUES
+(1, "Pranav", 'Bangalore'),
+(2, 'Rahul', 'Delhi'),
+(3, 'Sneha', 'Hyderabad'),
+(4, 'Anul', 'Chennai'),
+(5, 'Meera', 'Mumbai'),
+(6, 'Kiran', 'Bangalore'),
+(7, 'Roshan', 'Delhi');
