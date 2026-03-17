f3e8f9d949d0fa213002dba8ed1b92afca91e709
Pranav kumar reddy k <kondapurampranav@gmail.com>
2026-03-17 21:22:10 +0530

    Updated new Quaries

--- mysql.sql
+++ mysql.sql
@@ -1,17 +1,21 @@
 
-CREATE TABLE customers (
-    id INT PRIMARY KEY,
-    name VARCHAR(50),
-    city VARCHAR(50)
-);
+-- CREATE TABLE customers (
+--     id INT PRIMARY KEY,
+--     name VARCHAR(50),
+--     city VARCHAR(50)
+-- );
 
--- INSERTION
+-- -- INSERTION
 
-INSERT INTO customers (id, name, city) VALUES
-(1, "Pranav", 'Bangalore'),
-(2, 'Rahul', 'Delhi'),
-(3, 'Sneha', 'Hyderabad'),
-(4, 'Anul', 'Chennai'),
-(5, 'Meera', 'Mumbai'),
-(6, 'Kiran', 'Bangalore'),
-(7, 'Roshan', 'Delhi');
+-- INSERT INTO customers (id, name, city) VALUES
+-- (1, "Pranav", 'Bangalore'),
+-- (2, 'Rahul', 'Delhi'),
+-- (3, 'Sneha', 'Hyderabad'),
+-- (4, 'Anul', 'Chennai'),
+-- (5, 'Meera', 'Mumbai'),
+-- (6, 'Kiran', 'Bangalore'),
+-- (7, 'Roshan', 'Delhi');
+
+-- INSERT INTO customers (name, city) 
+-- values ('Naina', 'Jammu');
+
