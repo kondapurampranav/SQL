160f701192dd5269d0fad7c937bfcf43599c9b88
Pranav kumar reddy k <kondapurampranav@gmail.com>
2026-04-02 01:02:17 +0530

    sql commands

--- mysql.sql
+++ mysql.sql
@@ -1,21 +1,72 @@
+-- delete
+drop table tablename;
 
--- CREATE TABLE customers (
---     id INT PRIMARY KEY,
---     name VARCHAR(50),
---     city VARCHAR(50)
--- );
+select * from table_name;
 
--- -- INSERTION
+insert into table_name (,, , )
+values (), ();
 
--- INSERT INTO customers (id, name, city) VALUES
--- (1, "Pranav", 'Bangalore'),
--- (2, 'Rahul', 'Delhi'),
--- (3, 'Sneha', 'Hyderabad'),
--- (4, 'Anul', 'Chennai'),
--- (5, 'Meera', 'Mumbai'),
--- (6, 'Kiran', 'Bangalore'),
--- (7, 'Roshan', 'Delhi');
+-- select with conditions
 
--- INSERT INTO customers (name, city) 
--- values ('Naina', 'Jammu');
+select * from table_name where id = 1;
+select * from table_name where name = "king";
+select * from table_name where completed = true;
+select * from table_name where completed = 0;
+
+
+-- update 
+
+update books
+set name = "the kill"
+where id = 3;
+
+update books
+set completed = false
+where id = 7;
+
+update books
+set priority = "low"
+where id = 1;
+
+-- delete
+
+delete from books where id = 6;
+delete from books where id = 7;
+-- cant be done using other colums!!
+
+
+-- filtering
+
+select * from books where id > 3;
+select * from books where amount > 500;
+
+
+-- sorting
+
+select * from books 
+order by id;
+
+select * from books
+order by amount asc;
+
+select * from books
+order by amount desc;
+
+
+-- filter
+
+select * from books
+limit 2;
+
+
+-- aggredate functions
+
+select count(*) from books;
+select sum(amount) from books;
+select avg(amount) from books;
+select max(amount) from books;
+select min(amount) from books;
+
+
+
 
